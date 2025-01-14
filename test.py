81e3c95385201b9fef9984f7e9af7c70ffac9368
Ujjawal Garg <ujjawalgarg7@gmail.com>
2025-01-14 18:30:20 +0530

    combined logic

--- test.py
+++ test.py
@@ -1,4 +1,71 @@
 import cv2
+import numpy as np
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+import math
+from collections import defaultdict
+import pandas as pd
+
+def classifyAPoint(points, p, k=3):
+    distance = []
+
+    for group in points:
+        for feature in points[group]:
+            euclidean_distance = math.sqrt((feature[0]-p[0])**2 + (feature[1]-p[1])**2 + (feature[2]-p[2])**2)
+            distance.append((euclidean_distance, group))
+
+    distance = sorted(distance, key=lambda x: x[0])[:k]
+
+    freq = defaultdict(int)
+    for d in distance:
+        freq[d[1]] += 1
+
+    return max(freq, key=freq.get)
+
+
+def rgb_to_hex(rgb):
+    """Convert an RGB color to a hexadecimal string."""
+    return '#{:02x}{:02x}{:02x}'.format(rgb[0], rgb[1], rgb[2])
+
+def func(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
+    resized_image = cv2.resize(image, (100, 100))  
+
+    pixels = resized_image.reshape((-1, 3))
+
+    n_colors = 5  
+    gmm = GaussianMixture(n_components=n_colors, random_state=0)
+    gmm.fit(pixels)
+
+    cluster_colors = gmm.means_.astype(int)
+    cluster_weights = gmm.weights_
+
+    dominant_index = np.argmax(cluster_weights)
+    dominant_color = cluster_colors[dominant_index]
+    hex_code = rgb_to_hex(dominant_color)
+
+    print(f"Dominant Color (RGB): {dominant_color}")
+    print(f"Dominant Color (Hex): {hex_code}")
+
+    # Display the dominant color
+    plt.figure(figsize=(2, 2))
+    plt.imshow([[dominant_color / 255]])  # Normalize color for display
+    plt.title(f"Hex {hex_code}", fontsize=10)
+    plt.axis('off')
+    plt.show()
+
+
+    color_rgb = (int(dominant_color[0]),int(dominant_color[1]),int(dominant_color[2]))
+
+    data = pd.read_csv('color_data.csv')
+
+    color_dict = data.groupby('Color_Family').apply(lambda group: list(group[['R', 'G', 'B']].itertuples(index=False, name=None))).to_dict()
+
+
+    k = 3
+
+    print("\n\ncolor is : {}".format(classifyAPoint(color_dict, color_rgb, k)))
+
 
 # Initialize the camera
 cap = cv2.VideoCapture(0)
@@ -28,6 +95,7 @@ else:
             captured_frame = frame
             print("Image captured and stored in variable.")
             print(type(captured_frame))
+            func(captured_frame)
             
         elif key == ord('q'):  # Quit when 'q' is pressed
             print("Exiting without capturing.")
